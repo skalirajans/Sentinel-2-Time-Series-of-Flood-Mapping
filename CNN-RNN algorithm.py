cbc97a0b948335e0cf5ab80cd39ada21670e6bf5
Kalirajan S <160304522+skalirajans@users.noreply.github.com>
2025-11-02 19:12:29 +0530

    Update CNN-RNN algorithm.py

--- CNN-RNN algorithm.py
+++ CNN-RNN algorithm.py
@@ -1,1 +1,45 @@
+def hybrid_rnn_cnn(input_size=(128, 128, 3)):
+    inputs = Input(input_size)
+    
+    # Encoder
+    x = Conv2D(64, (3, 3), activation='relu', padding='same')(inputs)
+    x = BatchNormalization()(x)
+    x = Conv2D(64, (3, 3), activation='relu', padding='same')(x)
+    x = BatchNormalization()(x)
+    x = MaxPooling2D((2, 2))(x)
+
+    x = Conv2D(128, (3, 3), activation='relu', padding='same')(x)
+    x = BatchNormalization()(x)
+    x = Conv2D(128, (3, 3), activation='relu', padding='same')(x)
+    x = BatchNormalization()(x)
+    x = MaxPooling2D((2, 2))(x)
+    
+    # Bottleneck
+    x = Conv2D(256, (3, 3), activation='relu', padding='same')(x)
+    x = BatchNormalization()(x)
+    x = Dropout(0.1)(x)
+    
+    # ConvLSTM Layer
+    x = Reshape((1, 32, 32, 256))(x)
+    x = ConvLSTM2D(filters=128, kernel_size=(3, 3), padding='same', return_sequences=True)(x)
+    x = ConvLSTM2D(filters=128, kernel_size=(3, 3), padding='same', return_sequences=False)(x)
+    x = BatchNormalization()(x)
+    x = Dropout(0.1)(x)
+    
+    # Decoder
+    x = UpSampling2D((2, 2))(x)
+    x = Conv2D(128, (3, 3), activation='relu', padding='same')(x)
+    x = BatchNormalization()(x)
+    x = Conv2D(128, (3, 3), activation='relu', padding='same')(x)
+    x = BatchNormalization()(x)
+    
+    x = UpSampling2D((2, 2))(x) 
+    x = Conv2D(64, (3, 3), activation='relu', padding='same')(x)
+    x = BatchNormalization()(x)
+    x = Conv2D(64, (3, 3), activation='relu', padding='same')(x)
+    x = BatchNormalization()(x)
+    
+    outputs = Conv2D(4, (1, 1), activation='softmax')(x)
+    model = Model(inputs=[inputs], outputs=[outputs])
+    return model
 
